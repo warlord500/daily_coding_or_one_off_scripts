afba2e7e35fda38cbe0d72c1a4c60b44f90d3b61
jadon belezos(aka warlord500) <jjbelezos@gmail.com>
2022-12-09 23:30:55 -0800

    adding a completed daily programming problem

--- 2022/December/p12-09bitSwapEvenOdd-rust/src/main.rs
+++ 2022/December/p12-09bitSwapEvenOdd-rust/src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
-    println!("Hello, world!");
+	let num = 0b01111010u8;
+	//should run some code to accept a number.
+	//either a binary or 10 radix
+	
+    	
+    println!("{:#b}",evenBitSwap(num));
+
+
+    //full test 
+    for i in (0..=255) {
+        
+       println!("{:#b} : {:#b}",i,evenBitSwap(i));
+    }
+}
+
+fn evenBitSwap (number : u8) -> u8{
+		//well we know that we would have 8 bits and have to do 4 swaps?
+		// lets see we have ands,ors,xors,rotates,shifts, 
+		// maybe some kind of asm command? 
+		//lets check
+        //i guess probably not most efficent way to do this but we could 
+        //extract each bit at time. 
+    let mut bitArray = [0; 8];
+    for i in (0..8) {
+        bitArray[i] = (number >> i) & 0b00000001u8;
+    }
+    // uhh now swap evens and odds?
+    let mut outNum = 0;
+    for i in (0..8) {
+        
+            outNum = outNum | (bitArray[if(i%2==0){i+1}else{i-1}] << i); 
+    } 
+        outNum
 }
