25bc8b655cb61ec4d30715a44063ed7bf19cd7d5
jadon belezos(aka warlord500) <jjbelezos@gmail.com>
2022-12-11 22:38:30 -0800

    updated with new faster method

--- 2022/December/p12-09bitSwapEvenOdd-rust/src/main.rs
+++ 2022/December/p12-09bitSwapEvenOdd-rust/src/main.rs
@@ -14,14 +14,14 @@ fn main() {
     }
 }
 
-fn evenBitSwap (number : u8) -> u8{
+fn evenBitSwap (number : u8) -> u8 {
 		//well we know that we would have 8 bits and have to do 4 swaps?
 		// lets see we have ands,ors,xors,rotates,shifts, 
 		// maybe some kind of asm command? 
 		//lets check
         //i guess probably not most efficent way to do this but we could 
         //extract each bit at time. 
-    let mut bitArray = [0; 8];
+    /*let mut bitArray = [0; 8];
     for i in (0..8) {
         bitArray[i] = (number >> i) & 0b00000001u8;
     }
@@ -30,6 +30,12 @@ fn evenBitSwap (number : u8) -> u8{
     for i in (0..8) {
         
             outNum = outNum | (bitArray[if(i%2==0){i+1}else{i-1}] << i); 
-    } 
+    }*/ 
+    //above is my slow, but reliable probably easy to understand
+    //this is my faster single line of code. 
+    let oddBits =  0b01010101;
+    let  evenBits = 0b10101010;
+    let outNum = ((number & oddBits) << 1) | ((number & evenBits) >> 1);  
+        
         outNum
 }
